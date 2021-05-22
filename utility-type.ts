1c515500ec869b18b67b6383abba0965addd7599
yeonabogosipda <49853597+yeonabogosipda@users.noreply.github.com>
2021-05-22 16:20:57 +0900

    Update utility-type.ts

--- utility-type.ts
+++ utility-type.ts
@@ -1,16 +1,16 @@
-  interface Product{
+interface Product{
     id: number;
     name: string;
     price: number;
     brand: string;
     stock: number;
-  }
+}
 
-  function fetchProducts(): Promise<Product[]>{
+function fetchProducts(): Promise<Product[]>{
   
-  }
+}
   
-  type ShoppingItem = Pick<Product, 'id' | 'name' | 'price'>
-  function displayProductDetail(shoppingItem: Pick<Product, 'id' | 'name' | 'price'>){
+type ShoppingItem = Pick<Product, 'id' | 'name' | 'price'>
+function displayProductDetail(shoppingItem: Pick<Product, 'id' | 'name' | 'price'>){
   
-  }
+}
